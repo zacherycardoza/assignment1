72b945315d542034605ad48cc4d2389fd1e9599b
Zachary Cardoza <zcardoza@my.centennialcollege.ca>
2021-09-24 13:33:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/routes.index");
+
+var app = express();
+
+// view engine setup
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error", { title: "Error" });
+});
+
+app.listen(3000);
+console.log("Server running at http://localhost:3000/");
+module.exports = app;
---
+++ routes/routes.index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+/* GET about page. */
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About Me" });
+});
+/* GET projects page. */
+router.get("/projects", function (req, res, next) {
+  res.render("projects", { title: "Projects" });
+});
+/* GET services page. */
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+/* GET contact page. */
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+
+module.exports = router;
